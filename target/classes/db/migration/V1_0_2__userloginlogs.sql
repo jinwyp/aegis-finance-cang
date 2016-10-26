371d053d8641560700a20650f87229339f757640
liuxinjie@123 <liuxinjie@yimei180.com>
2016-10-26 15:39:11 +0800

    delete

--- target/classes/db/migration/V1_0_2__userloginlogs.sql
+++
@@ -1,8 +0,0 @@
-
-CREATE TABLE `f_warehouse_userlogin_log` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `create_time` datetime NOT NULL COMMENT '创建时间',
-  `user_id` varchar(30) NOT NULL COMMENT '用户id',
-  `username` varchar(30) NOT NULL COMMENT '用户登录名',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户登陆日志表';
