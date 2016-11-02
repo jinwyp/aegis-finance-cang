25175f8923a6afca8d0a9e11c86bee831ec27b9d
liuxinjie@123 <liuxinjie@yimei180.com>
2016-11-02 17:28:50 +0800

    user

--- src/main/java/com.yimei.warehouse/AegisWareHouseApplication.java
+++ src/main/java/com.yimei.warehouse/AegisWareHouseApplication.java
@@ -1,21 +1,17 @@
 package com.yimei.warehouse;
 
-import com.yimei.warehouse.config.session.AdminSession;
-import com.yimei.warehouse.representation.admin.user.UserObject;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.Date;
 
 @SpringBootApplication
 @Slf4j
 public class AegisWareHouseApplication {
+
     public static void main(String[] args) throws UnknownHostException {
         SpringApplication app = new SpringApplication(AegisWareHouseApplication.class);
         Environment env = app.run(args).getEnvironment();
@@ -24,15 +20,16 @@ public class AegisWareHouseApplication {
                         "External: \thttp://{}:{}\n----------------------------------------------------------",
                 env.getProperty("server.port"), InetAddress.getLocalHost().getHostAddress(),
                 env.getProperty("server.port"));
+//        adminSession.login(new UserObject(String.valueOf(1), "admin", "超级管理员", "15618177577", "1134005157@qq.com", new Date()));
     }
 
-    @Bean
-    public CommandLineRunner init(AdminSession adminSession) {
-        return new CommandLineRunner() {
-            @Override
-            public void run(String... strings) throws Exception {
-                adminSession.login(new UserObject(String.valueOf(1), "admin", "超级管理员", "15618177577", "1134005157@qq.com", new Date()));
-            }
-        };
-    }
+//    @Bean
+//    public CommandLineRunner init(AdminSession adminSession) {
+//        return new CommandLineRunner() {
+//            @Override
+//            public void run(String... strings) throws Exception {
+//
+//            }
+//        };
+//    }
 }
