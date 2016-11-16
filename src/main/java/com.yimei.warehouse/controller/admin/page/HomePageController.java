33fcfe6dbb30c820ac084fd9e80e7886b5f5f2b5
JinWYP <jinwyp@gmail.com>
2016-11-16 18:55:01 +0800

    update

--- src/main/java/com.yimei.warehouse/controller/admin/page/HomePageController.java
+++ src/main/java/com.yimei.warehouse/controller/admin/page/HomePageController.java
@@ -15,26 +15,56 @@ public class HomePageController {
     private AdminSession adminSession;
 
     /**
-     * 管理首页 页面
+     * 后台管理首页 页面
      */
     @ApiOperation(value = "管理后台管理员首页", notes = "管理后台管理员首页 需要管理员登录")
     @RequestMapping(value = "/warehouse/admin/home", method = RequestMethod.GET)
-    public String index() {
+    public String adminHome() {
         if (adminSession.getUser() == null) {
             return "redirect:/warehouse/admin/login";
         }
         return "admin/home";
     }
 
-    /**
-     * 管理页面 为了前端路由需要重定向
-     */
     @ApiOperation(value = "管理后台前端路由重定向", notes = "管理页面为了前端路由需要重定向")
     @RequestMapping(value = "/warehouse/admin/home/**", method = RequestMethod.GET)
-    public String adminHome() {
+    public String adminHomeRedirect() {
         if (adminSession.getUser() == null) {
             return "redirect:/warehouse/admin/login";
         }
         return "forward:/warehouse/admin/home";
     }
+
+
+    @ApiOperation(value = "管理后台首页跳转", notes = "管理后台首页跳转 没有登录调转到登录页面, 如果登录了跳转到管理员Home页面")
+    @RequestMapping(value = "/warehouse/admin", method = RequestMethod.GET)
+    public String adminIndexRedirect() {
+        if (adminSession.getUser() == null) {
+            return "redirect:/warehouse/admin/login";
+        }
+        return "redirect:/warehouse/admin/home";
+    }
+
+
+    @ApiOperation(value = "管理后台登陆页面", notes = "管理后台登陆页面 不需要登录就可以访问")
+    @RequestMapping(value = "/warehouse/admin/login", method = RequestMethod.GET)
+    public String adminLogin() {
+        return "admin/login";
+    }
+
+
+
+    @ApiOperation(value = "管理后台form页面", notes = "管理后台form页面")
+    @RequestMapping(value = "/warehouse/admin/form", method = RequestMethod.GET)
+    public String formPage() {
+        return "admin/home";
+    }
+
+
+    @ApiOperation(value = "管理后台list页面", notes = "管理后台list页面")
+    @RequestMapping(value = "/warehouse/admin/listPage", method = RequestMethod.GET)
+    public String listPage() {
+        return "admin/listPage";
+    }
+
 }
