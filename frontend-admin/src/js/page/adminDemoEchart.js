559bf5dac3be6853af8a1af8fc283615b8089b53
JinWYP <jinwyp@gmail.com>
2016-11-25 14:15:13 +0800

    update

--- frontend-admin/src/js/page/adminDemoEchart.js
+++ frontend-admin/src/js/page/adminDemoEchart.js
@@ -6,8 +6,6 @@
 
 var avalon = require('avalon2') ;
 var $ = require('jquery') ;
-require('bootstrap/dist/js/bootstrap.js');
-require('../component/header.js');
 
 
 var chart = function() {
---
+++ frontend-admin/src/js/page/adminHeader.js
@@ -0,0 +1,9 @@
+/**
+ * Created by JinWYP on 25/11/2016.
+ */
+
+
+var $ = require('jquery') ;
+require('bootstrap/dist/js/bootstrap.js');
+require('../component/header.js');
+
